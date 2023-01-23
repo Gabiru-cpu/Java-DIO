826c1453088ec49ef5a1af84e2d7ceed90b9d6fd
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-01-23 02:23:40 -0300

    desafios basicos java

---
+++ primeiro-projeto-java/src/edu/gabriel/DesafiosBasicos/Desafios.java
@@ -0,0 +1,82 @@
+package edu.gabriel.DesafiosBasicos;
+
+import java.util.Scanner;
+
+public class Desafios {
+
+    public static void main(String[] args) {
+
+    }
+    // public class Program {
+    // public static void main(String[] args) {
+    // Scanner leitor = new Scanner(System.in);
+    // int doce = leitor.nextInt();
+
+    // // TODO: Crie a condição necessária para que cada cliente saiba quantos doces
+    // vai obter de acordo com a entrada
+    // System.out.println("O cliente obteve " + (doce * 2) + " doces");
+    // }
+    // }
+
+    // public class Program {
+    // public static void main(String[] args) {
+    // Scanner leitor = new Scanner(System.in);
+    // int paginas = leitor.nextInt();
+    // int paginasLidas = 3;
+
+    // int result = paginas / paginasLidas;
+    // // TODO: Crie a condição necessária para que Getrudes saiba em quanto tempo
+    // terminará seus livros
+
+    // System.out.println(result + " dias");
+    // }
+    // }
+
+    // Seu sobrinho começou a ganhar mesada dos pais. No auge dos
+    // seus 8 anos recebendo 50 reais por mês, ele quer saber quanto
+    // terá se juntar todo o dinheiro, sem gastar nada, durante alguns
+    // meses. E você, como um bom tio com habilidades de programação,
+    // vai criar um programa que com as entradas do seu sobrinho vai
+    // calcular automaticamente a quantidade de dinheiro que ele terá
+    // em X meses.
+    // import java.util.Scanner;
+
+    // public class Program {
+    // public static void main(String[] args) {
+    // Scanner leitor = new Scanner(System.in);
+    // int entrada = leitor.nextInt();
+    // int mesada = 50;
+    // int result = 0;
+
+    // // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro
+    // irá juntar nos meses da entrada
+    // result = mesada * entrada;
+    // System.out.println(result);
+    // }
+    // }
+
+    // Você foi contratado para criar um gerenciador de pacotes. Porém, o que restou
+    // para você
+    // fazer foi o contador de porcentagem de download dos pacotes. Mas como o
+    // espaço para essa
+    // informação ficou pequeno, a empresa optou por fazer uma escala de 10 – 1.
+    // Então a escala
+    // será 1 = 10%, 2 = 20%, 3 = 30% e etc.
+    // Para cada 10% de download, o programa deve printar, em sequência e sem
+    // espaços, uma barra
+    // “/”.
+
+    // public class Program {
+    // public static void main(String[] args) {
+    // Scanner leitor = new Scanner(System.in);
+    // int tamanho = leitor.nextInt();
+
+    // // TODO: Crie a condição necessária para que, de acordo com o tamanho, seja
+    // printado no console barras representando o download
+    // for(int i=0; i < tamanho; i++)
+    // {
+    // System.out.print("/");
+    // }
+    // }
+    // }
+}
--- primeiro-projeto-java/src/edu/gabriel/EntendendoMétodosJava/EX2/Main.java
+++ primeiro-projeto-java/src/edu/gabriel/EntendendoMétodosJava/EX2/Main.java
@@ -3,11 +3,27 @@ package edu.gabriel.EntendendoMétodosJava.EX2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d, 5d);
-        Quadrilatero.area(7, 8, 9);
-        Quadrilatero.area(5f, 5f);
+        // chamada de metodos
+        // System.out.println("Exercício quadrilátero");
+        // Quadrilatero.area(3);
+        // Quadrilatero.area(5d, 5d);
+        // Quadrilatero.area(7, 8, 9);
+        // Quadrilatero.area(5f, 5f);
+
+        // metodos com retorno
+        System.out.println("Exercício quadrilátero com retorno");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Area do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5d, 5d);
+        System.out.println("Area do quadrado: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Area do quadrado: " + areaTrapezio);
+
+        double areaLosango = Quadrilatero.area(5f, 5f);
+        System.out.println("Area do quadrado: " + areaLosango);
     }
 
 }
